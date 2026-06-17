81113c0047748992d527878da6136791ef8ad501
HCJ <875287034@qq.com>
2026-06-17 22:42:57 +0800

    新增Hello.java文件

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main{
+	public static void main(String[] args){
+		Scanner in = new Scanner(System.in);
+		System.out.print("tell me what's your name:");
+		String name = in.next();
+		System.out.println("hello," + name);
+
+	}
+}
